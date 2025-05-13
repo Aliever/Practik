1b52448018d296375a24133cc10e1e8ae7a8ad5a
Aliever <emie71040@gmail.com>
2025-05-13 13:47:50 +0700

    777

--- practik2.1/task2.py
+++ practik2.1/task2.py
@@ -1,25 +1,24 @@
 from itertools import combinations #юзал чтоб генерить комбинации
 
-def comb_sum(numbers, target):
+def comb_sum(candidates, target):
     result = set()  #чтоб  хранить ток уникальные комбо
 
 
-    for number in numbers:   # Проверяем одиночные элементы
+    for number in candidates:   # Проверяем одиночные элементы
         if number == target:
             result.add((number,))  # Добавляем одиночный элемент как кортеж
 
     # Генерируем комбинации разной длины
-    for r in range(2, len(numbers) + 1):  # Начинаем с 2 до длины списка
-        for combo in combinations(numbers, r):  #r определит какого размера будут генериться комбинации
+    for r in range(2, len(candidates) + 1):  # Начинаем с 2 до длины списка
+        for combo in combinations(candidates, r):  #r определит какого размера будут генериться комбинации
             if sum(combo) == target:
                 result.add(combo)  # Добавляем комбинацию в множество
 
     return list(result)  #множество => список
 
-
-numbers = [1, 2, 2, 4, 5]
+candidates = [1, 2, 2, 4, 5]
 target = 5
-combinations_found = comb_sum(numbers, target)
+combinations_found = comb_sum(candidates, target)
 print(combinations_found)
 
 
--- practik2.1/task3.py
+++ practik2.1/task3.py
@@ -1,12 +1,24 @@
+
 def contains_duplicate(nums):
     seen = []  #пустой список для отслеживания встреченных чисел
     for num in nums:
         if num in seen:  # проверяем, есть ли число в списке
             return True
         seen.append(num)  # Если нет, добавляем число в список
+def duplicate(nums):
+    a = []  #пустой список для отслеживания встреченных чисел
+    for num in nums:
+        if num in a:  # проверяем, есть ли число в списке
+            return True
+        a.append(num)  # Если нет, добавляем число в список
+
     return False
 
 # Примеры использования:
 numbers = [1, 1, 3, 5, 6]
+
 print(contains_duplicate(numbers))
+
+print(duplicate(numbers))
+
 
